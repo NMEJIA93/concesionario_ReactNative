d2145cdc59b60e7e75d78ab799059a4f1d1ba22e
nmejia93 <norbeymejiacortes@gmail.com>
2024-03-16 11:57:04 -0500

    unificacion 2

--- concesionario_ReactNative/App.jsx
+++ concesionario_ReactNative/App.jsx
@@ -1,30 +1,9 @@
 import React from 'react'
-
 import Navigation from './src/navigation/Navigation'
-import { View, } from 'react-native'
-import TechnicalServiceScreen from './src/screens/TechnicalServiceScreen'
-import TechnicalServiceFormScreen  from "./src/screens/TechnicalServiceFormScreen";
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-import QuoteScreen from './src/screens/QuoteScreen';
-
-
-
 
 const App = () => {
   return (
-
-    /*     <Navigation/> */
-  
-
-    <SafeAreaProvider>
-     {/*  <QuoteScreen/>  */}
-       <TechnicalServiceScreen/>   
-     {/*  <TechnicalServiceFormScreen/> */}
-    </SafeAreaProvider>
-
-
-    //<QuoteScreen/>
-    //<ServiceHistory/>
+    <Navigation/>
   )
 }
 
